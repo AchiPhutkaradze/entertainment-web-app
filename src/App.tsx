54bfffdb8e996b0e57e33e82c9b552b055789f32
achiPhutkaradze <achifutkaradze9@gmail.com>
2024-04-02 13:32:37 +0400

    Add: validation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
+import Home from "./components/Home";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,49 @@
-import { useEffect } from "react";
 import loginIcon from "../assets/images/film-icon.svg";
 import { Link } from "react-router-dom";
-type emailAndPass = {
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+interface emailAndPass {
   email: string;
   password: string;
-};
+}
 export default function Login() {
+  const [correctEmailAndPass, setCorrectEmailAndPass] =
+    useState<boolean>(false);
+  console.log(correctEmailAndPass);
   const inputStyle = {
     borderBottom: "1px solid #5A698F",
   };
+  const [validationObj, setValidationObj] = useState<emailAndPass>({
+    email: "",
+    password: "",
+  });
+
   useEffect(() => {
-    const mail: emailAndPass | string = JSON.parse(
+    let emailAndPass: emailAndPass | any = JSON.parse(
       localStorage.getItem("Email") || ""
     );
-    console.log(mail);
+    setValidationObj(emailAndPass);
   }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<emailAndPass>();
+  const onSubmit = (data: emailAndPass) => {
+    console.log(data);
+    if (
+      data.email === validationObj["email"] &&
+      data.password === validationObj["password"]
+    ) {
+      setCorrectEmailAndPass(true);
+    } else {
+      setCorrectEmailAndPass(false);
+    }
+  };
+
+  console.log("Error", errors);
+
   return (
     <div className=" h-lvh bg-bgLogin">
       <div className="flex pt-12 flex-col gap-y-14  items-center">
@@ -23,23 +52,39 @@ export default function Login() {
           <div className="pl-6 pt-6">
             <h1 className="text-white size-8 leading-10 font-light">Login</h1>
           </div>
-          <form className=" flex flex-col gap-y-6 mt-10 pl-6 pr-6">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className=" flex flex-col gap-y-6 mt-10 pl-6 pr-6"
+          >
             <input
               style={inputStyle}
               className="bg-bgLoginCard pl-4 h-inputHeight outline-none text-white"
               placeholder="Email address"
+              {...register("email")}
             />
             <input
               style={inputStyle}
               className="bg-bgLoginCard pl-4 h-inputHeight outline-none text-white"
               placeholder="Password"
+              {...register("password")}
             />
-            <button
-              onClick={(e) => e.preventDefault()}
-              className=" bg-bgLoginBtn  w-full text-center pt-4 pb-4 rounded-md mt-10 text-white"
-            >
-              Login to your account
-            </button>
+            {correctEmailAndPass ? (
+              <Link to={`/home`}>
+                <button
+                  type="submit"
+                  className=" bg-bgLoginBtn  w-full text-center pt-4 pb-4 rounded-md mt-10 text-white"
+                >
+                  Login to your account
+                </button>
+              </Link>
+            ) : (
+              <button
+                type="submit"
+                className=" bg-bgLoginBtn  w-full text-center pt-4 pb-4 rounded-md mt-10 text-white"
+              >
+                Login to your account
+              </button>
+            )}
           </form>
           <div className="pt-6 pb-8 flex  justify-center">
             <p className="text-white">
